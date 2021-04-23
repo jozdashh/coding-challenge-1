ca8f59c7218adbfbad93fefab5d7f9e7818e55e8
Josue Pena <jozdashh@javerianacali.edu.co>
2021-04-23 15:20:00 -0500

    Handled edge cases

--- n-grams.py
+++ n-grams.py
@@ -5,18 +5,22 @@
 # N: amount of characters in 'text'
 # n: amount of n-grams
 
-# Upper bound time-complexity: O(N) + O(N-n) + O(n) = O(N)
+# Even if n = N, time complexity is still bounded by N
+
+# Upper bound time complexity: O(N) + O(N-n) + O(n) = O(N)
 def calculate_n_grams(text, n):
+    assert 0 < n <= len(text)
     ngrams = list()
     # Remove leading and trailing whitespace
     text = text.strip() # O(N)
     for i in range(len(text)-n+1): # O(N-n)
-        ngrams.append(text[i:i+n]) # O(n) Python list slices depend on both indices: i+n - i = n
+        ngrams.append(text[i:i+n]) # O(n) Python list slices depend on both indices: (i+n) - (i) = n
     return ngrams
 
 
-# Upper bound time-complexity: O(N) + O(N-n) = O(N)
+# Upper bound time complexity: O(N) + O(N-n) = O(N)
 def most_frequent_n_gram(text, n):
+    assert 0 < n <= len(text)
     ngrams = calculate_n_grams(text,n) # O(N)
     count = dict()
     current_max = 1
