660f444ca4ba6d7815aa95791b95379591c950e1
Josue Peña <jozdashh@javerianacali.edu.co>
2021-04-29 15:29:19 -0500

    Fixed O-notation mistake and spelling

--- n-grams.py
+++ n-grams.py
@@ -7,21 +7,21 @@
 
 # Even if n = N, time complexity is still bounded by N
 
-# Upper bound time complexity: O(N) + O(N-n) + O(n) = O(N)
+# Upper bound time complexity: O(N) + O(N-n)*O(n)
 def calculate_n_grams(text, n):
     assert 0 < n <= len(text)
     ngrams = list()
     # Remove leading and trailing whitespace
     text = text.strip() # O(N)
     for i in range(len(text)-n+1): # O(N-n)
-        ngrams.append(text[i:i+n]) # O(n) Python list slices depend on both indices: (i+n) - (i) = n
+        ngrams.append(text[i:i+n]) # O(n) Python list slices depend on both indexes: (i+n) - (i) = n
     return ngrams
 
 
-# Upper bound time complexity: O(N) + O(N-n) = O(N)
+# Upper bound time complexity: O(N) + O(N-n) + O(N-n)*O(n)
 def most_frequent_n_gram(text, n):
     assert 0 < n <= len(text)
-    ngrams = calculate_n_grams(text,n) # O(N)
+    ngrams = calculate_n_grams(text,n) # O(N) + O(N-n)*O(n)
     count = dict()
     current_max = 1
     answer = ngrams[0]
